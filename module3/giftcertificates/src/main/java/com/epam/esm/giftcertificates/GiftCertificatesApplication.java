8bf0c783068fbe57e65b14ada01771b2c44419be
Yevhen Pashchenko <eipas9@gmail.com>
2023-05-20 19:34:25 +0300

    Update for build war file

--- module3/giftcertificates/src/main/java/com/epam/esm/giftcertificates/GiftCertificatesApplication.java
+++ module3/giftcertificates/src/main/java/com/epam/esm/giftcertificates/GiftCertificatesApplication.java
@@ -2,11 +2,18 @@ package com.epam.esm.giftcertificates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class GiftCertificatesApplication {
+public class GiftCertificatesApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GiftCertificatesApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GiftCertificatesApplication.class, args);
